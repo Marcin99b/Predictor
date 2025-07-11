fa1669e9020cab9544137ef1021409aec69b1f69
Marcin99b <marcin.bigos99@gmail.com>
2025-07-12 00:36:06 +0200

    refactor

--- src/Predictor.Web/Models/CalculateInput.cs
+++ src/Predictor.Web/Models/CalculateInput.cs
@@ -2,34 +2,12 @@
 
 public record CalculateInput(decimal InitialBudget, MonthDate StartCalculationMonth, PaymentItem[] Incomes, PaymentItem[] Outcomes)
 {
-    private bool CheckRecurring(MonthDate month, MonthDate StartDate, RecurringConfig? recurringConfig)
-    {
-        if (recurringConfig == null || StartDate > month)
-        {
-            return false;
-        }
+    public IEnumerable<PaymentItem> GetMonthIncomes(MonthDate month) 
+        => this.Incomes.Where(x => this.Filter(month, x));
 
-        if (recurringConfig.EndDate != null && recurringConfig.EndDate < month)
-        {
-            return false;
-        }
+    public IEnumerable<PaymentItem> GetMonthOutcomes(MonthDate month) 
+        => this.Outcomes.Where(x => this.Filter(month, x));
 
-        var calculatedMonth = this.StartCalculationMonth;
-        while (calculatedMonth < month)
-        {
-            calculatedMonth = calculatedMonth.AddMonths(recurringConfig.MonthInterval);
-        }
-
-        return calculatedMonth == month;
-    }
-
-    public IEnumerable<PaymentItem> GetMonthIncomes(MonthDate month)
-    {
-        return this.Incomes.Where(x => x.StartDate == month || this.CheckRecurring(month, x.StartDate, x.RecurringConfig));
-    }
-
-    public IEnumerable<PaymentItem> GetMonthOutcomes(MonthDate month)
-    {
-        return this.Outcomes.Where(x => x.StartDate == month || this.CheckRecurring(month, x.StartDate, x.RecurringConfig));
-    }
+    private bool Filter(MonthDate month, PaymentItem item) 
+        => item.StartDate == month || item.CheckRecurring(month, this.StartCalculationMonth);
 }
--- src/Predictor.Web/Models/PaymentItem.cs
+++ src/Predictor.Web/Models/PaymentItem.cs
@@ -1,3 +1,25 @@
 ﻿namespace Predictor.Web.Models;
 
-public record PaymentItem(string Name, decimal Value, MonthDate StartDate, RecurringConfig? RecurringConfig = null);
+public record PaymentItem(string Name, decimal Value, MonthDate StartDate, RecurringConfig? RecurringConfig = null)
+{
+    public bool CheckRecurring(MonthDate month, MonthDate startCalculationMonth)
+    {
+        if (this.RecurringConfig == null || this.StartDate > month)
+        {
+            return false;
+        }
+
+        if (this.RecurringConfig.EndDate != null && this.RecurringConfig.EndDate < month)
+        {
+            return false;
+        }
+
+        var calculatedMonth = startCalculationMonth;
+        while (calculatedMonth < month)
+        {
+            calculatedMonth = calculatedMonth.AddMonths(this.RecurringConfig.MonthInterval);
+        }
+
+        return calculatedMonth == month;
+    }
+}
