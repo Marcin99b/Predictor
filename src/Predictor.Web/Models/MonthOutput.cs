d88789e9461b7b736ec78b60a05c77c037c5b3b8
Marcin99b <marcin.bigos99@gmail.com>
2025-07-12 16:55:02 +0200

    fix namespace

--- src/Predictor.Web/Models/MonthOutput.cs
+++ src/Predictor.Web/Models/MonthOutput.cs
@@ -1,3 +1,3 @@
-﻿using Predictor.Web.Models;
+﻿namespace Predictor.Web.Models;
 
 public record MonthOutput(MonthDate MonthDate, decimal BudgetAfter, decimal Balance, decimal Income, decimal Outcome);
